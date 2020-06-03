8e37f1ee9c0929148db5fa70b232c90a4e70c704
Guilherme <guilhermet213@gmail.com>
2020-06-03 15:29:22 -0300

    Add GET method for items entity

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,20 @@
-import express from 'express';
+import express, { request } from 'express';
+import knex from './database/connection';
 
 const routes = express.Router();
 
-routes.get('/users', (req, res) => {
-    return res.json({ message: 'Hello World' });
+routes.get('/items', async (req, res) => {
+  const items = await knex('items').select('*');
+
+  const serializedItems = items.map((item) => {
+    return {
+      id: item.id,
+      title: item.title,
+      image_url: `http://localhost:3000/uploads/${item.image}`,
+    };
+  });
+
+  return res.json(serializedItems);
 });
 
 export default routes;
