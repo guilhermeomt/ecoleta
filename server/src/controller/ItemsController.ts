f872df383d7281dfd520f642adce1b3feea38d2b
Guilherme <guilhermet213@gmail.com>
2020-06-05 18:29:16 -0300

    Update API image url to a github hosting service
    Used GithubGist to host images. reason: to mobile app consume it propertly

--- server/src/controller/ItemsController.ts
+++ server/src/controller/ItemsController.ts
@@ -7,9 +7,9 @@ class ItemsController {
 
         const serializedItems = items.map((item) => {
             return {
-                id: item.id,
+                id: item.id,  
                 title: item.title,
-                image_url: `http://localhost:3000/uploads/${item.image}`,
+                image_url: `https://gist.githubusercontent.com/guilhermeomt/4b2f779ef9e1e034e8a8f8a8601871e9/raw/d7cb11dafb27bac3b76a3ba9f3ce934bbc17c4b8/${item.image}`,
             };
         });
 
