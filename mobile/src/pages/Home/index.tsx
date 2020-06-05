af765a7d584fb42915cd29b2068bede414519f66
Guilherme <guilhermet213@gmail.com>
2020-06-05 19:28:42 -0300

    Add mobile app points view page

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -2,8 +2,15 @@ import React from 'react';
 import { Feather as Icon } from '@expo/vector-icons'
 import { StyleSheet, Text, View, ImageBackground, Image } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native';
 
 const Home = () => {
+    const navigation = useNavigation();
+
+    function handleNavigateToPoints() {
+      navigation.navigate('Points');
+    }
+
     return (
         <ImageBackground
             source={require('../../assets/home-background.png')}
@@ -18,7 +25,7 @@ const Home = () => {
             </View>
 
             <View style={styles.footer}></View>
-            <RectButton style={styles.button} onPress={() => { }}>
+            <RectButton style={styles.button} onPress={handleNavigateToPoints}>
                 <View style={styles.buttonIcon}>
                     <Icon name="arrow-right" color="#FFF" size={24}></Icon>
                 </View>
@@ -35,7 +42,6 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         padding: 32,
-        backgroundColor: '#F0F0F5'
     },
 
     main: {
