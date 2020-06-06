f37ce606d3825b34f9eb12ada9ce6f469c944b20
Guilherme <guilhermet213@gmail.com>
2020-06-06 01:40:00 -0300

    Fix API points query
    Also updated database with two points tuples

--- server/src/controller/PointsController.ts
+++ server/src/controller/PointsController.ts
@@ -8,7 +8,7 @@ class PointsController {
         const parsedItems = String(items).split(',').map(item => Number(item.trim()));
 
         const points = await knex('points')
-        .join('point_items', 'point_id', '=', 'point_items.point_id')
+        .join('point_items', 'points.id', '=', 'point_items.point_id')
         .whereIn('point_items.item_id', parsedItems)
         .where('city', String(city))
         .where('uf', String(uf))
