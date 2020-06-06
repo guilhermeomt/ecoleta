8fb0ddf91645e91b7a40a64ae794affc52bdadb3
Guilherme <guilhermet213@gmail.com>
2020-06-06 18:41:58 -0300

    Update frontend with dropzone to upload images

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,14 +1,15 @@
-import React, { useEffect, useState, ChangeEvent, FormEvent } from 'react';
-import { Link, useHistory } from 'react-router-dom';
-import { FiArrowLeft } from 'react-icons/fi';
 import axios from 'axios';
-import Swal from 'sweetalert2';
-import { Map, TileLayer, Marker } from 'react-leaflet';
 import { LeafletMouseEvent } from 'leaflet';
-
-import './styles.css';
+import React, { ChangeEvent, FormEvent, useEffect, useState } from 'react';
+import { FiArrowLeft } from 'react-icons/fi';
+import { Map, Marker, TileLayer } from 'react-leaflet';
+import { Link, useHistory } from 'react-router-dom';
+import Swal from 'sweetalert2';
 import logo from '../../assets/logo.svg';
+import Dropzone from '../../components/dropzone';
 import api from '../../services/api';
+import './styles.css';
+
 
 interface Item {
     id: number,
@@ -42,6 +43,7 @@ const CreatePoint = () => {
     const [selectedCity, setSelectedCity] = useState('0');
     const [selectedItems, setSelectedItems] = useState<number[]>([]);
     const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0]);
+    const [selectedFile, setSelectedFile] = useState<File>();
 
     const history = useHistory();
 
@@ -117,26 +119,30 @@ const CreatePoint = () => {
         const [latitude, longitude] = selectedPosition;
         const items = selectedItems;
 
-        const data = {
-            name,
-            email,
-            whatsapp,
-            uf,
-            city,
-            latitude,
-            longitude,
-            items
-        };
+        const data = new FormData();
+
+        data.append('name', name);
+        data.append('email', email);
+        data.append('whatsapp', whatsapp);
+        data.append('uf', uf);
+        data.append('city', city);
+        data.append('latitude', String(latitude));
+        data.append('longitude', String(longitude));
+        data.append('items', items.join(','));
+
+        if(selectedFile) {
+            data.append('image', selectedFile);
+        }
 
         await api.post('points', data);
-        
+
         Swal.fire({
             position: 'center',
             icon: 'success',
             title: 'Cadastro concluído!',
             showConfirmButton: false,
             timer: 1500
-          })
+        })
         setTimeout(() => history.push('/'), 2000);
     }
 
@@ -155,6 +161,8 @@ const CreatePoint = () => {
             <main>
                 <form onSubmit={handleSubmit}>
                     <h1>Cadastro do <br /> ponto de coleta</h1>
+
+                    <Dropzone onFileUploaded={setSelectedFile} />
 
                     <fieldset>
                         <legend>
